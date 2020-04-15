d6952b2346fd3371edabc999e3f48be6152e0080
jscastanos <jec.castanos@gmail.com>
2020-04-15 23:49:51 +0800

    Challenge Number 3  - Error Handling

---
+++ 3 - Error Handling/Program.cs
@@ -0,0 +1,54 @@
+﻿using DemoLibrary;
+using System;
+
+namespace _3___Error_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                try
+                {
+                    var result = paymentProcessor.MakePayment($"Demo{ i }", i);
+
+                    if (result != null)
+                    {
+                        Console.WriteLine(result.TransactionAmount);
+                    }
+                    else
+                    {
+                        throw new NullReferenceException($"Null value for item {i}");
+                    }
+
+                }
+                catch (NullReferenceException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Skipped invalid record  {(ex.InnerException != null ? ex.InnerException.Message : "")}");
+                }
+                catch (FormatException ex) when (i != 5)
+                {
+                    Console.WriteLine($"Formatting Issue {(ex.InnerException != null ? ex.InnerException.Message : "")}");
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine($"Payment skipped for payment with { i } items  {(ex.InnerException != null ? ex.InnerException.Message : "")}");
+                }
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
