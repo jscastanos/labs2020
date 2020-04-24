58535d347039c906fd82ae681d23590be0d34d8a
jscastanos <jec.castanos@gmail.com>
2020-04-24 23:12:44 +0800

    Challenge  6 - DateTime (partial)
    Add Date Function,
    TODO Time Function

---
+++ 5 - DateTime/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _6___DateTime
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            string[] options = new string[3] { "Date", "Time", "Exit" };
+
+            Console.WriteLine("DateTime - Challenge");
+
+            while (true)
+            {
+                switch (SelectOptions("Please Select: ", options))
+                {
+                    case 1:
+                        CalculateDate();
+                        break;
+                    case 2:
+                        CalculateTime();
+                        break;
+                    case 3:
+                        return;
+                }
+            }
+        }
+
+        static void CalculateDate()
+        {
+            Regex dateRX = new Regex(@"^(\d{1,2})-(\d{1,2})-(\d{1,4})$");
+
+            string[] format = new string[2] { "MM-dd-yyyy", "dd-MM-yyyy" };
+            int formatIndex = SelectOptions("Select Date Format: ", format);
+            string dateStr;
+
+            Console.WriteLine("Please not that the system only accepts \"-\" or dash as date separator");
+            Console.Write($"Enter Date ({format[formatIndex - 1]}): ");
+
+            dateStr = Console.ReadLine();
+
+            if (dateRX.IsMatch(dateStr))
+            {
+                var splitDate = dateStr.Split('-').Select(Int32.Parse).ToArray();
+
+                try
+                {
+                    DateTime newDate;
+
+                    if (formatIndex == 1)
+                        newDate = Convert.ToDateTime(splitDate[0] + "-" + splitDate[1] + "-" + splitDate[2]);
+                    else
+                        newDate = Convert.ToDateTime(splitDate[1] + "-" + splitDate[0] + "-" + splitDate[2]);
+
+                    Console.WriteLine($"It has been { (DateTime.Now - newDate).Days } days since { newDate.ToString(format[formatIndex - 1])} \n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message} \n");
+                }
+            }
+            else Console.WriteLine("Invalid Date\n");
+
+
+        }
+
+
+        static void CalculateTime()
+        {
+            //    Regex timeRx1 = new Regex(@"^(\d{1,2}):(\d{1,2})$");
+            //    Regex timeRx2 = new Regex(@"^(\d{1,2}):(\d{1,2}) ([AP]M)$");
+
+            //    string[] formats = new string[2] { "12 hr (AM/PM)", "24 hr" };
+            //    int formatIndex = SelectOptions("Select Time Format", formats);
+            //    string timeStr;
+
+            //    Console.Write($"Enter a ({formats[formatIndex - 1]}) time: ");
+            //    timeStr = Console.ReadLine();
+
+            //    if (timeRx1.IsMatch(timeStr))
+            //    {
+            //        var splitTime = timeStr.Split(':').Select(Int32.Parse).ToArray();
+
+            //        if (splitTime[0] > 12 && formatIndex == 1)
+            //            Console.WriteLine("Invalid 12 hr time");
+            //        else
+            //        {
+            //            if (formatIndex == 1)
+            //            {
+            //                bool IsAM =
+            //            }
+
+            //            //TimeSpan newTime,
+            //            //         timeDiff;
+
+            //            //newTime = new TimeSpan(splitTime[0], splitTime[1], 0);
+            //            //timeDiff = DateTime.Now.TimeOfDay -
+            //        }
+
+            //    }
+            //    else Console.WriteLine("Invalid Time\n");
+            Console.WriteLine("TIME");
+
+        }
+
+        static int SelectOptions(string message, string[] options)
+        {
+            while (true)
+            {
+
+                for (var index = 0; index < options.Length; index++)
+                    Console.WriteLine($"[{index + 1}] - {options[index]}");
+
+                Console.Write("\n" + message);
+
+                try
+                {
+                    int command = Convert.ToInt32(Console.ReadLine());
+
+                    if (command > 0 && command <= options.Length)
+                        return command;
+                    else
+                        Console.WriteLine("Invalid Command\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid Command: {ex.Message}\n");
+                }
+
+
+            }
+        }
+    }
+}
