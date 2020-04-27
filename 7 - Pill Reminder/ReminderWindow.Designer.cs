a6b53060003fcf5cec2bababc807ff20a7fb518c
jscastanos <jec.castanos@gmail.com>
2020-04-27 16:37:41 +0800

    Challenge 7 - Pill Reminder

---
+++ 7 - Pill Reminder/ReminderWindow.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PillReminderUI
+{
+    partial class ReminderWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.allPillsListBox = new System.Windows.Forms.ListBox();
+            this.pillsToTakeListBox = new System.Windows.Forms.ListBox();
+            this.pillsToTakeLabel = new System.Windows.Forms.Label();
+            this.allPillsLabel = new System.Windows.Forms.Label();
+            this.takePill = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // allPillsListBox
+            // 
+            this.allPillsListBox.FormattingEnabled = true;
+            this.allPillsListBox.ItemHeight = 29;
+            this.allPillsListBox.Location = new System.Drawing.Point(480, 116);
+            this.allPillsListBox.Name = "allPillsListBox";
+            this.allPillsListBox.Size = new System.Drawing.Size(310, 294);
+            this.allPillsListBox.TabIndex = 0;
+            // 
+            // pillsToTakeListBox
+            // 
+            this.pillsToTakeListBox.FormattingEnabled = true;
+            this.pillsToTakeListBox.ItemHeight = 29;
+            this.pillsToTakeListBox.Location = new System.Drawing.Point(59, 116);
+            this.pillsToTakeListBox.Name = "pillsToTakeListBox";
+            this.pillsToTakeListBox.Size = new System.Drawing.Size(310, 294);
+            this.pillsToTakeListBox.TabIndex = 0;
+            // 
+            // pillsToTakeLabel
+            // 
+            this.pillsToTakeLabel.AutoSize = true;
+            this.pillsToTakeLabel.Location = new System.Drawing.Point(53, 80);
+            this.pillsToTakeLabel.Name = "pillsToTakeLabel";
+            this.pillsToTakeLabel.Size = new System.Drawing.Size(156, 29);
+            this.pillsToTakeLabel.TabIndex = 1;
+            this.pillsToTakeLabel.Text = "Pills To Take";
+            // 
+            // allPillsLabel
+            // 
+            this.allPillsLabel.AutoSize = true;
+            this.allPillsLabel.Location = new System.Drawing.Point(474, 80);
+            this.allPillsLabel.Name = "allPillsLabel";
+            this.allPillsLabel.Size = new System.Drawing.Size(174, 29);
+            this.allPillsLabel.TabIndex = 1;
+            this.allPillsLabel.Text = "Medication List";
+            // 
+            // takePill
+            // 
+            this.takePill.Location = new System.Drawing.Point(120, 423);
+            this.takePill.Name = "takePill";
+            this.takePill.Size = new System.Drawing.Size(169, 73);
+            this.takePill.TabIndex = 2;
+            this.takePill.Text = "Take Pill";
+            this.takePill.UseVisualStyleBackColor = true;
+            this.takePill.Click += new System.EventHandler(this.TakePill_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // ReminderWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 530);
+            this.Controls.Add(this.takePill);
+            this.Controls.Add(this.allPillsLabel);
+            this.Controls.Add(this.pillsToTakeLabel);
+            this.Controls.Add(this.pillsToTakeListBox);
+            this.Controls.Add(this.allPillsListBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.Name = "ReminderWindow";
+            this.Text = "Pill Reminder Challenge by Tim Corey";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox allPillsListBox;
+        private System.Windows.Forms.ListBox pillsToTakeListBox;
+        private System.Windows.Forms.Label pillsToTakeLabel;
+        private System.Windows.Forms.Label allPillsLabel;
+        private System.Windows.Forms.Button takePill;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 7 - Pill Reminder/ReminderWindow.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PillReminderUI
+{
+    public partial class ReminderWindow : Form
+    {
+        readonly BindingList<PillModel> medications = new BindingList<PillModel>();
+        BindingList<PillModel> pillsToTake = new BindingList<PillModel>();
+
+        public ReminderWindow()
+        {
+            InitializeComponent();
+            allPillsListBox.DataSource = medications;
+            allPillsListBox.DisplayMember = "PillInfo";
+
+            PopulateDummyData();
+        }
+
+        private void PopulateDummyData()
+        {
+            medications.Add(new PillModel { PillName = "The white one", TimeToTake = DateTime.ParseExact("0:15 am", "h:mm tt", null, System.Globalization.DateTimeStyles.AssumeLocal) });
+            medications.Add(new PillModel { PillName = "The big one", TimeToTake = DateTime.Parse("8:00 am") });
+            medications.Add(new PillModel { PillName = "The red ones", TimeToTake = DateTime.Parse("11:45 pm") });
+            medications.Add(new PillModel { PillName = "The oval one", TimeToTake = DateTime.Parse("0:27 am") });
+            medications.Add(new PillModel { PillName = "The round ones", TimeToTake = DateTime.Parse("6:15 pm") });
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            List<PillModel> temp = new List<PillModel>();
+
+            foreach (PillModel medication in medications)
+            {
+                if (DateTime.Now.TimeOfDay >= medication.TimeToTake.TimeOfDay && medication.LastTaken.Date != DateTime.Now.Date)
+                    temp.Add(medication);
+            }
+
+            if (temp.Count() != pillsToTake.Count())
+            {
+                pillsToTake = new BindingList<PillModel>(temp);
+                pillsToTakeListBox.DataSource = pillsToTake.OrderBy(o => o.TimeToTake).ToList(); ;
+                pillsToTakeListBox.DisplayMember = "PillInfo";
+            }
+        }
+
+        private void TakePill_Click(object sender, EventArgs e)
+        {
+            var item = pillsToTakeListBox.SelectedItem;
+
+            foreach (PillModel medication in medications)
+            {
+                if (medication.Equals(item))
+                {
+                    medication.LastTaken = DateTime.Now;
+                    pillsToTake.Remove(medication);
+                    pillsToTakeListBox.DataSource = pillsToTake;
+                    break;
+                }
+            }
+
+        }
+    }
+}
