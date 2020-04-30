fe5d5fa5976b5271c7cbf8df99ab95ac6dc23796
jscastanos <jec.castanos@gmail.com>
2020-04-30 21:34:04 +0800

    Initial commit

---
+++ Controllers/v1/PostController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TweetBook.Contracts;
+using TweetBook.Contracts.v1.Request;
+using TweetBook.Contracts.v1.Response;
+using TweetBook.Domain;
+
+namespace TweetBook.Controllers
+{
+    public class PostController : Controller
+    {
+        private List<Post> _posts;
+
+        public PostController()
+        {
+            _posts = new List<Post>();
+            for (int i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
+            }
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public IActionResult Create([FromBody] CreatePostRequest postRequest)
+        {
+            var post = new Post { Id = postRequest.Id };
+
+            if (string.IsNullOrEmpty(post.Id))
+                post.Id = Guid.NewGuid().ToString();
+
+            _posts.Add(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
+
+            var reponse = new PostResponse { Id = post.Id };
+
+            return Created(locationUri, reponse);
+        }
+
+    }
+}
---
+++ Installers/MvcInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetBook.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Tweetbok API", Version = "v1" });
+            });
+        }
+    }
+}
