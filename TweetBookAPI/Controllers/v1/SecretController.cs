b3ca3d1e19feb103026e2444c0b59719b1082188
jscastanos <jec.castanos@gmail.com>
2020-05-14 23:38:30 +0800

    Created ApiKeyBased Auth
    I'm still looking for a way to have a user-specific api key

---
+++ TweetBookAPI/Controllers/v1/SecretController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TweetBookAPI.Filters;
+
+namespace TweetBookAPI.Controllers.v1
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("Secret got exposed");
+        }
+    }
+}
