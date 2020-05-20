fecb4172a0b1943e3c545ee6a325d565bfd2890c
jscastanos <jec.castanos@gmail.com>
2020-05-20 12:39:34 +0800

    Added pages and home view

--- src/products/models.py
+++ src/products/models.py
@@ -5,3 +5,4 @@ class Product(models.Model):
     name        = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(max_digits=10000, decimal_places=2)
+    featured    = models.BooleanField()
