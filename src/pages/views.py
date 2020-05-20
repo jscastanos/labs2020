1f75296cdf8e0b5ba08b8dc29aa3bc54323f857b
jscastanos <jec.castanos@gmail.com>
2020-05-20 12:51:20 +0800

    Added templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+
+def contact_view(*args, **kwargs):
+    return render(request, "contact.html", {})
+    
+def about_view(*args, **kwargs):
+    return render(request, "about.html", {})
