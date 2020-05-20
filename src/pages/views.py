ef5453c82bcaacd77d1cca65b829bc00ecc8c2de
jscastanos <jec.castanos@gmail.com>
2020-05-20 13:00:47 +0800

    Added base.html

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
     
-def about_view(*args, **kwargs):
+def about_view(request, *args, **kwargs):
     return render(request, "about.html", {})
