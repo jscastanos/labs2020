052d7286a373ad49c57cae8786f64758b8d2feb9
jscastanos <jec.castanos@gmail.com>
2020-05-21 13:46:47 +0800

    Added article model in admin site, created superuser

--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = CharField(max_length=100),
-    slug  = SlugField(),
-    body  = TextField(),
-    date  = DateField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    slug  = models.SlugField()
+    body  = models.TextField()
+    date  = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
