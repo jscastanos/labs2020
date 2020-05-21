dc2f94eb5e2b833fcb73ba61f9c3e758208c6473
jscastanos <jec.castanos@gmail.com>
2020-05-21 13:21:48 +0800

    Added Article Model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = CharField(max_length=100),
+    slug  = SlugField(),
+    body  = TextField(),
+    date  = DateField(auto_now_add=True)
