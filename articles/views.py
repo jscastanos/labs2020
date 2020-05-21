4ee2f92737267b0da0446c9349c62e28e2b083b5
jscastanos <jec.castanos@gmail.com>
2020-05-21 14:46:15 +0800

    Added static files

--- articles/views.py
+++ articles/views.py
@@ -4,4 +4,4 @@ from .models import Article
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
-    return render(request, 'articles/article_list.html', {'articles': articles, 'title': 'These are the articles'})
+    return render(request, 'articles/article_list.html', {'articles': articles})
