d0785611b0fb29d964a35317d45a022b28e71c38
jscastanos <jec.castanos@gmail.com>
2020-05-21 19:44:17 +0800

    Added login redirection

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
-from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def article_list(request):
@@ -10,3 +10,7 @@ def article_list(request):
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+@login_required(login_url="accounts:login")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
