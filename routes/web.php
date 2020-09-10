91db87e5f0d22a6896cb79218df43562726ebae1
jscastanos <jec.castanos@gmail.com>
2020-09-11 03:55:30 +0800

    Add new route and view, pass some data to views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    $pizza = ['type' => 'hawaiian', 'base' => 'cheesy crust', 'price' => 16];
+    return view('pizzas', $pizza);
+});
