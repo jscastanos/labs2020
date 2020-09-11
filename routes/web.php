cf0f3e72e74be1e6caf29fbad0651b6277168e51
jscastanos <jec.castanos@gmail.com>
2020-09-11 19:22:43 +0800

    Post request

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Route::get('/pizzas', [PizzaController::class, 'index']);
 Route::get('/pizzas/create', [PizzaController::class, 'create']);
+Route::post('/pizzas', [PizzaController::class, 'store']);
 Route::get('/pizzas/{id}', [PizzaController::class, 'show']);
