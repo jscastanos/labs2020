dc00d193897ce48df0a5185b6f21bda715a45abe
jscastanos <jec.castanos@gmail.com>
2020-09-11 20:18:44 +0800

    SASS

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="wrapper pizza-details">
-        <h1>Order for {{ $pizza->name }}</h1>
-        <p class="type">Type - {{ $pizza->type }}</p>
-        <p class="base">Base - {{ $pizza->base }}</p>
-        <p class="toppings">Extra Toppings:</p>
-        <ul>
-            @foreach ($pizza->toppings as $topping)
-                <li>{{ $topping }}</li>
-            @endforeach
-        </ul>
-        <form action="/pizzas/{{ $pizza->id }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button>Complete order</button>
-        </form>
+    <div>
+        <div class="wrapper pizza-details">
+            <h1>Order for {{ $pizza->name }}</h1>
+            <p class="type">Type - {{ $pizza->type }}</p>
+            <p class="base">Base - {{ $pizza->base }}</p>
+            <p class="toppings">Extra Toppings:</p>
+            <ul>
+                @foreach ($pizza->toppings as $topping)
+                    <li>{{ $topping }}</li>
+                @endforeach
+            </ul>
+            <form action="/pizzas/{{ $pizza->id }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button>Complete order</button>
+            </form>
+        </div>
+        <a href="/pizzas" class="back">
+            <- Back to all pizzas</a>
     </div>
-    <a href="/pizzas" class="back">
-        <- Back to all pizzas </a>
-        @endsection
+@endsection
