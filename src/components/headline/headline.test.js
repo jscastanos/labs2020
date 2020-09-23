508d6651ade5b8689fa8ea57cc8cf2a85870fcdf
jscastanos <jec.castanos@gmail.com>
2020-09-23 15:43:52 +0800

    Added Headline Component and Test Cases

---
+++ src/components/headline/headline.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Headline from './index';
+
+import { findByTestAttr } from "../../Utils";
+
+const setup = (props) => {
+    const component = shallow(<Headline {...props} />);
+    return component;
+}
+
+describe('Headline Component', () => {
+    describe('Have props', () => {
+        let wrapper;
+
+        beforeEach(() => {
+            const props = {
+                header: 'Test Header',
+                desc : 'Test Desc'
+            };
+            wrapper = setup(props);
+        });
+
+        it('Should render without errors', () => {
+            const component = findByTestAttr(wrapper, 'HeadlineComponent');
+            expect(component.length).toBe(1);
+        });
+
+        it('Should render a header', () => {
+            const header = findByTestAttr(wrapper, 'header');
+            expect(header.length).toBe(1);
+        });
+
+        it('Should render a desc', () => {
+            const desc = findByTestAttr(wrapper, 'desc');
+            expect(desc.length).toBe(1);
+        })
+    });
+
+    describe('Have NO props', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            wrapper = setup();
+        });
+
+        
+        it('Should not render component', () => {
+            const component = findByTestAttr(wrapper, 'HeadlineComponent');
+            expect(component.length).toBe(0);
+        });
+
+        it('Should not render a header', () => {
+            const header = findByTestAttr(wrapper, 'header');
+            expect(header.length).toBe(0);
+        });
+
+        it('Should not render a desc', () => {
+            const desc = findByTestAttr(wrapper, 'desc');
+            expect(desc.length).toBe(0);
+        })
+    });
+});
