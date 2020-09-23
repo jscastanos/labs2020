362797313c891ce1f8b5c7ce229f5f4f7053c7ed
jscastanos <jec.castanos@gmail.com>
2020-09-23 14:43:28 +0800

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from "./components/header/index"
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-    
+      <Header></Header>
     </div>
   );
 }
