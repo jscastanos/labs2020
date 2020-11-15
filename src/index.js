053b03a8f613adb26c1ad752f1bf8b4b713d2529
Jec Castanos <jec.castanos@gmail.com>
2020-11-15 16:35:20 +0800

    Immutability

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import { pipe } from 'lodash/fp';
+const person = {
+  name: 'John',
+  address: {
+    country: 'USA',
+    city: 'San Francisco',
+  },
+};
+const updated = {
+  ...person,
+  address: {
+    ...person.address,
+    city: 'NewYork',
+  },
+  name: 'Bob',
+};
 
-let input = ' JavaScript   ';
-let output = '<div>' + input.trim() + '</div>';
-
-// const wrapInDiv = (str) => `<div>${str}</div>`;
-// const wrapInSpan = (str) => `<span>${str}</span>`;
-
-// currying
-// instead of repeating the function
-// we simplify it to
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
-
-const transformPipe = pipe(trim, toLowerCase, wrap('div'));
-console.log(transformPipe(input));
+console.log(person);
+console.log(updated);
