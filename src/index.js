d8713e1f2e79d4e7448beca20ac89fa15b8aecd8
Jec Castanos <jec.castanos@gmail.com>
2020-11-15 16:23:03 +0800

    currying

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import { compose, pipe } from 'lodash/fp';
+import { pipe } from 'lodash/fp';
 
 let input = ' JavaScript   ';
 let output = '<div>' + input.trim() + '</div>';
 
+// const wrapInDiv = (str) => `<div>${str}</div>`;
+// const wrapInSpan = (str) => `<span>${str}</span>`;
+
+// currying
+// instead of repeating the function
+// we simplify it to
 const trim = (str) => str.trim();
-const wrapInDiv = (str = `<div>${str}</div>`);
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
 const toLowerCase = (str) => str.toLowerCase();
 
-// compose is equivalent to
-// const result = wrapInDiv(toLowerCase(trim(input)))
-// but much cleaner
-const transform = compose(wrapInDiv, toLowerCase, trim);
-transform(input);
-
-// same with compose but the order is flipped
-const transformPipe = pipe(trim, toLowerCase, wrapInDiv);
-transformPipe(input);
+const transformPipe = pipe(trim, toLowerCase, wrap('div'));
+console.log(transformPipe(input));
