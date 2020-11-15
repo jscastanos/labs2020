07b875cb55da95777adbbfdf6309290572cb2bae
Jec Castanos <jec.castanos@gmail.com>
2020-11-15 18:29:07 +0800

    action creators

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-import * as actions from './actionTypes';
+import { bugAdded, bugRemoved } from './actions';
 import store from './store';
 
 const unsubscribe = store.subscribe(() => {
   console.log('Store changed!', store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: 'Bug1',
-  },
-});
+store.dispatch(bugAdded('Bug 1'));
 
 unsubscribe();
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugRemoved(1));
 
 console.log(store.getState());
