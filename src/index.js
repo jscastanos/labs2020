f592a08d5c782715f6ccb5b23376161b8847b438
Jec Castanos <jec.castanos@gmail.com>
2020-11-15 16:39:43 +0800

    array immutability

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-const person = {
-  name: 'John',
-  address: {
-    country: 'USA',
-    city: 'San Francisco',
-  },
-};
-const updated = {
-  ...person,
-  address: {
-    ...person.address,
-    city: 'NewYork',
-  },
-  name: 'Bob',
-};
+const numbers = [1, 2, 3];
 
-console.log(person);
+// Adding
+const index = numbers.indexOf(2);
+const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
+
+console.log(added);
+
+// Removing
+const removed = numbers.filter((n) => n !== 2);
+
+console.log(removed);
+
+// updating
+const updated = numbers.map((n) => (n === 2 ? 20 : n));
 console.log(updated);
