77de4fd3c8a1272967f48780ff50c719f0fe7dc9
Jec Castanos <jec.castanos@gmail.com>
2020-11-15 16:10:03 +0800

    composing and piping

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { compose, pipe } from 'lodash/fp';
+
 let input = ' JavaScript   ';
 let output = '<div>' + input.trim() + '</div>';
 
@@ -5,4 +7,12 @@ const trim = (str) => str.trim();
 const wrapInDiv = (str = `<div>${str}</div>`);
 const toLowerCase = (str) => str.toLowerCase();
 
-result = wrapInDiv(toLowerCase(trim(input)));
+// compose is equivalent to
+// const result = wrapInDiv(toLowerCase(trim(input)))
+// but much cleaner
+const transform = compose(wrapInDiv, toLowerCase, trim);
+transform(input);
+
+// same with compose but the order is flipped
+const transformPipe = pipe(trim, toLowerCase, wrapInDiv);
+transformPipe(input);
